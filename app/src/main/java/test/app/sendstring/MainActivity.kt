fbb3d6c8f9e26da817ac56d633a430b07ec59b42
Yana <44026935+yanajulias@users.noreply.github.com>
2023-11-23 04:17:11 +0700

    Final code

--- app/src/main/java/test/app/sendstring/MainActivity.kt
+++ app/src/main/java/test/app/sendstring/MainActivity.kt
@@ -1,21 +1,23 @@
 package test.app.sendstring
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import test.app.sendstring.ui.theme.SendStringTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,34 +31,68 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-class ServiceManager {
-    fun startService(callback: (String) -> Unit) {
-        val data = "Ini data dari service"
-        callback(data)
-    }
+interface DataCallback {
+    fun onDataReceived(data: String)
 }
 
 @Composable
 fun SendStringApp() {
-    var dataReceived by remember { mutableStateOf("Tidak ada data") }
-    val serviceManager = ServiceManager()
+    val context = LocalContext.current
+    val receivedData = remember { mutableStateOf("") }
+    val serviceStarted = remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(
-            onClick = {
-                serviceManager.startService {
-                    dataReceived = it
+
+        if (!serviceStarted.value) {
+            Button(
+                onClick = {
+                    serviceStarted.value = true
+                    Toast.makeText(context, "Service sudah aktif", Toast.LENGTH_SHORT).show()
                 }
+            ) {
+                Text(text = "Start Service")
             }
-        ) {
-            Text(text = "Start Service")
+        } else {
+            DataServiceContent(callback = object : DataCallback {
+                override fun onDataReceived(data: String) {
+                    receivedData.value = data
+                }
+            })
         }
 
-        Text(text = dataReceived)
+        if (receivedData.value.isNotBlank()) {
+            ShowReceivedData(receivedData.value)
+        }
+    }
+}
+
+@Composable
+fun DataServiceContent(callback: DataCallback) {
+    ServiceContent(callback)
+}
+
+@Composable
+fun ShowReceivedData(data: String) {
+    Text(
+        text = "Data yang diterima dari service: $data",
+        modifier = Modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun ServiceContent(callback: DataCallback) {
+    Column {
+        Button(
+            onClick = {
+                callback.onDataReceived("Ini data yang dikirim dari service")
+            }
+        ) {
+            Text(text = "Kirim Data ke Activity")
+        }
     }
 }
 
