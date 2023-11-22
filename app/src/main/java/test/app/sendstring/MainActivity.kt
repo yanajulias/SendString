2225f582bd363bef410705694fbc1402dd72fa59
Yana <44026935+yanajulias@users.noreply.github.com>
2023-11-23 03:07:06 +0700

    Send data using callback

--- app/src/main/java/test/app/sendstring/MainActivity.kt
+++ app/src/main/java/test/app/sendstring/MainActivity.kt
@@ -3,11 +3,17 @@ package test.app.sendstring
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import test.app.sendstring.ui.theme.SendStringTheme
@@ -17,30 +23,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SendStringTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                SendStringApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+class ServiceManager {
+    fun startService(callback: (String) -> Unit) {
+        val data = "Ini data dari service"
+        callback(data)
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun SendStringApp() {
+    var dataReceived by remember { mutableStateOf("Tidak ada data") }
+    val serviceManager = ServiceManager()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                serviceManager.startService {
+                    dataReceived = it
+                }
+            }
+        ) {
+            Text(text = "Start Service")
+        }
+
+        Text(text = dataReceived)
+    }
+}
+
+@Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     SendStringTheme {
-        Greeting("Android")
+        SendStringApp()
     }
 }
